d1d4bf03fc5f679bb15a9cbc6ef95277fed0b77f
Evgeny Levinsky <evgeny@ideal-code.com>
2025-10-31 14:16:37 -0500

    Extracting orders from commerce by runtime action

--- actions/commerce/orders/index.js
+++ actions/commerce/orders/index.js
@@ -1,9 +1,32 @@
-const { HTTP_OK } = require('/lib/constants')
+const { fetchOrders } = require('/lib/commerce')
+const { HTTP_OK, HTTP_INTERNAL_ERROR } = require('/lib/constants')
 
-exports.main = async function main() {
+exports.main = async function main(params) {
+  let orders = {}
+
+  let request = {
+    'searchCriteria[currentPage]': params.current_page || 1,
+    'searchCriteria[pageSize]': params.page_size || 1000
+  }
+  if (params.order_ids) {
+    request['searchCriteria[filterGroups][0][filters][0][field]'] = 'entity_id'
+    request['searchCriteria[filterGroups][0][filters][0][value]'] = params.order_ids
+    request['searchCriteria[filterGroups][0][filters][0][condition_type]'] = 'in'
+  }
+
+  try {
+    orders = await fetchOrders(params, request)
+  } catch (err) {
+    return {
+      statusCode: HTTP_INTERNAL_ERROR,
+      body: {
+        error: err.message
+      }
+    }
+  }
+
   return {
     statusCode: HTTP_OK,
-    body: {
-    }
+    body: orders
   }
 }
