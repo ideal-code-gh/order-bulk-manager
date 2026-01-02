8cbc8bcd417f0c71b52469547daf27152e1acdd3
Evgeny Levinsky <evgeny@ideal-code.com>
2026-01-02 12:23:31 -0600

    Updated linter for reading all js including web src

--- src/commerce-backend-ui-1/web-src/src/main.js
+++ src/commerce-backend-ui-1/web-src/src/main.js
@@ -1,20 +1,19 @@
 import { register } from '@adobe/uix-guest'
-import _ from 'lodash'
 
 const extensionId = 'order-bulk-manager'
-const frontBaseURL = _.trimEnd(process.env.FRONTEND_BASE_URL, '/')
+const frontBaseURL = process.env.FRONTEND_BASE_URL.replace(/\/+$/, '')
 
 register({
-  id: extensionId
+  id: extensionId,
 }).then(async () => {
   const {
-    default: App
+    default: App,
   } = await import(`${frontBaseURL}/${extensionId}.js`)
 
   App({
     backend: {
       baseURL: process.env.BACKEND_BASE_URL,
-      accessToken: process.env.BACKEND_ACCESS_TOKEN
-    }
+      accessToken: process.env.BACKEND_ACCESS_TOKEN,
+    },
   })
 })
